0fa4dc012f1e17ee3da96e59a977316e9861f244
PuiSlate <pkaraket73@gmail.com>
2025-07-21 13:01:30 -0500

    Add files via upload

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -27,7 +27,10 @@ Think about which debugging methods you found most useful and how you might appl
 console.log("Welcome to the bootcamp
 
 // What’s Wrong?
-
+//Syntax error: missing closing qoutation, an exclamation mark or full stop, closing parethesis and a semi colon.
+//Debugging method: reading error messages
+//The correct code shoule be:
+console.log("Welcome to the bootcamp."); 
 
 // Program B
 // Description:
@@ -40,7 +43,18 @@ for (let i = 0; i < numbers.length; i++) {
 }
 
 // What’s Wrong?
-
+//Runtime error: "eight" is string, when it's multiplied by two the result is NaN
+//Debugging method: consol.log()
+//We can either change the array to be all number or skip the non-numeric value as per below;
+let numbers = [2, 4, "eight"];
+for (let i = 0; i < numbers.length; i++) {
+  if (typeof numbers[i] === 'number') {
+    let doubled = numbers[i] * 2;
+    console.log(doubled);
+  } else {
+    console.log(`Skipping non-numeric value: ${numbers[i]}`);
+  }
+}
 
 
 // Program C (Logic Error)
@@ -60,3 +74,18 @@ function isPrime(num) {
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+//logic error in the for loop. When num % i === 0 is true, it means num is divisible by i,
+//(meaning it has a factor other than 1 and itself), and therefore is not a prime number.
+//However, the function returns 'true' which is falsely indicating that num is prime.
+//Here's the correct version.
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;  // It's divisible by i, so it's not prime
+    }
+  }
+  return true; // No divisors found, it's prime
+}
+
+console.log(isPrime(7));  // true
